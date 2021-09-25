6ef2de4a8f7ce3280cf2f594b1bab327645141da
kritish bhattarai <rishi58rishi@gmail.com>
2021-09-25 17:29:52 +0545

    update

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const app = express();
 dotenv.config();
 connectDb();
 
+app.get('/', (req, res) => {
+   res.json({ success: true });
+});
+
 app.listen(process.env.PORT || 5000, () => {
    console.log('Server is running on port:', process.env.PORT || 5000);
 });
